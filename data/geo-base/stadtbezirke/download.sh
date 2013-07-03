0ae7d1e1fecf34d6fcbad1a1f7637e5b17b0b5d4
Marian Steinbach <marian@sendung.de>
2013-07-03 16:40:20 +0200

    Updated download script

--- data/geo-base/stadtbezirke/download.sh
+++ data/geo-base/stadtbezirke/download.sh
@@ -1,15 +1,18 @@
 SOURCE_URL="http://www.offenedaten-koeln.de/wp-content/plugins/download-monitor/download.php?id=14"
-FILENAME="Stadtbezirk.zip"
-NEW_FOLDER="Stadtbezirk_new"
-FINAL_FOLDER="Stadtbezirk"
+NAME="Stadtbezirk"
+FINAL_FOLDER=$NAME
+FILENAME=${NAME}.zip
+NEW_FOLDER=${NAME}_new
 USER_AGENT="datahub-cgn/0.1"
 
-rm -r _source/$NEW_FOLDER
+echo "Downloading $NAME - $FILENAME to $NEW_FOLDER"
+rm -rf _source/$NEW_FOLDER
 mkdir -p _source/$NEW_FOLDER
 cd _source/$NEW_FOLDER
 wget -U $USER_AGENT -O $FILENAME $SOURCE_URL
 unzip $FILENAME
 rm $FILENAME
 cd ..
-rm -r $FINAL_FOLDER
+rm -rf $FINAL_FOLDER
 mv $NEW_FOLDER $FINAL_FOLDER
+cd ..
