b101866f7c23d1b4e809835db7fb5e8df60a5533
Marian Steinbach <marian@sendung.de>
2013-07-03 17:01:53 +0200

    Files swapped with automatically downloaded sources

---
+++ data/geo-base/strassen/download.sh
@@ -0,0 +1,18 @@
+SOURCE_URL="http://www.offenedaten-koeln.de/wp-content/plugins/download-monitor/download.php?id=63"
+NAME="Strasse"
+FINAL_FOLDER=$NAME
+FILENAME=${NAME}.zip
+NEW_FOLDER=${NAME}_new
+USER_AGENT="datahub-cgn/0.1"
+
+echo "Downloading $NAME - $FILENAME to $NEW_FOLDER"
+rm -rf _source/$NEW_FOLDER
+mkdir -p _source/$NEW_FOLDER
+cd _source/$NEW_FOLDER
+wget -U $USER_AGENT -O $FILENAME $SOURCE_URL
+unzip $FILENAME
+rm $FILENAME
+cd ..
+rm -rf $FINAL_FOLDER
+mv $NEW_FOLDER $FINAL_FOLDER
+cd ..
