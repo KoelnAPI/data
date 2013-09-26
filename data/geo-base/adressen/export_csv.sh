940abb878c57f803acfa72f947c613836d483b9f
Marian Steinbach <marian@sendung.de>
2013-09-26 12:28:24 +0200

    More verbose comments for export_csv.sh

--- data/geo-base/adressen/export_csv.sh
+++ data/geo-base/adressen/export_csv.sh
@@ -1,5 +1,10 @@
 # Convert ESRI Shapefile to CSV
+#
+# ogr2ogr is part of the GDAL software suite,
+# avaiable at http://www.gdal.org/
 
+# export shapefile data including point coordinates in WGS84
+# format (lon/lat) to intermediate file
 ogr2ogr -f CSV adressen_temp.csv _source/Adressen/Adresse.shp -lco GEOMETRY=AS_XY -t_srs "EPSG:4326"
 
 # You might want to check the output file manually
@@ -9,7 +14,11 @@ ogr2ogr -f CSV adressen_temp.csv _source/Adressen/Adresse.shp -lco GEOMETRY=AS_X
 # Otherwise, corrupt CSV will be generated
 # in the sort step.
 
+# write only first row (column names) to result file
 head -n 1 adressen_temp.csv > adressen.csv
-tail -n+2 adressen_temp.csv|sort -t"," -k3 >> adressen.csv
-rm adressen_temp.csv
 
+# sort all other rows and append them to the result file
+tail -n+2 adressen_temp.csv|sort -t"," -k3 >> adressen.csv
+
+# delete intermediate file
+rm adressen_temp.csv
