0ec32b2e0944d6657c9b77d6adfa783a9f9d52f4
Marian Steinbach <marian@sendung.de>
2013-10-09 19:23:34 +0200

    Download script now creates improved CSV version

--- data/geo-base/wahllokale/download.sh
+++ data/geo-base/wahllokale/download.sh
@@ -13,8 +13,12 @@ NEW_FOLDER=${NAME}_new
 USER_AGENT="datahub-cgn/0.1"
 
 # single file download
-echo "Downloading $NAME - $FILENAME to $NEW_FOLDER"
+echo "Downloading $NAME to $FILENAME"
 rm -rf ./_source/*
 cd ./_source/
 wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
 cd ..
+
+# remove dots from within figures, replace seperator
+echo "Creating improved version in $FILENAME"
+cat _source/$FILENAME|in2csv -f csv -d ";"  > $FILENAME
