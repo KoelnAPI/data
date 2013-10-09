f74ed73ab1f2f28b33a5504546b842140933c88c
Marian Steinbach <marian@sendung.de>
2013-10-09 18:03:19 +0200

    New dataset: finanzplan_produktbereiche

---
+++ data/budget/finanzplan_produktbereiche/download.sh
@@ -0,0 +1,23 @@
+# Download script for downloading the source in the current folder.
+#
+# Execute this script from within the same directory as work directory, like this:
+#   
+#   sh ./download.sh
+
+
+SOURCE_URL="http://www.offenedaten-koeln.de/wp-content/plugins/download-monitor/download.php?id=9"
+NAME="finanzplan_produktbereiche"
+FINAL_FOLDER=$NAME
+FILENAME=${NAME}.csv
+USER_AGENT="datahub-cgn/0.1"
+
+echo "Downloading $NAME to _source/$FILENAME"
+rm -rf _source/*
+mkdir -p _source
+cd _source
+wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
+cd ..
+
+# remove dots from within figures
+#cat _source/$FILENAME|sed 's/\([0-9]\)\.\([0-9]\)/\1\2/g'|sed 's/;/,/g' > $FILENAME
+cat _source/$FILENAME|sed 's/\([0-9]\)\.\([0-9]\)/\1\2/g' > $FILENAME
