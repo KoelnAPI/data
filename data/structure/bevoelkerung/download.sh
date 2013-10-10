8b9b5995cd841b63ec43ace57526a6013e5e66e0
Marian Steinbach <marian@sendung.de>
2013-10-10 12:13:23 +0200

    Improved download.sh now checks whether source has changed

--- data/structure/bevoelkerung/download.sh
+++ data/structure/bevoelkerung/download.sh
@@ -11,6 +11,12 @@ FINAL_FOLDER=$NAME
 FILENAME=${NAME}.csv
 USER_AGENT="datahub-cgn/0.1"
 
+# creating SHA1 hash to compare before/after download
+if [ -a _source/$FILENAME ]; then
+    HASH=shasum.before
+    shasum _source/$FILENAME > $HASH
+fi
+
 echo "Downloading $NAME to _source/$FILENAME"
 rm -rf _source/*
 mkdir -p _source
@@ -18,9 +24,22 @@ cd _source
 wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
 cd ..
 
+# Check SHA1 of new download
+if [ -a $HASH ]; then
+    shasum -c --status $HASH
+    if [ $? -eq 0 ]; then
+        echo "Source file hasn't changed"
+        rm $HASH
+        exit
+    else
+        echo "Source file has been updated"
+        rm $HASH
+    fi
+fi
+
 # standardize format, remove unnecessary columns
 echo "Creating improved version in $FILENAME"
 perl -CSD -pe 'tr/\x{feff}//d' _source/$FILENAME > $FILENAME.tmp
-in2csv -f csv -d ";" $FILENAME.tmp > $FILENAME.tmp2 && rm $FILENAME.tmp
+in2csv -f csv -d ";" -y 100000 $FILENAME.tmp > $FILENAME.tmp2 && rm $FILENAME.tmp
 csvcut -C 2,3,11,12 $FILENAME.tmp2 > $FILENAME.tmp3 && rm $FILENAME.tmp2
 mv $FILENAME.tmp3 $FILENAME
