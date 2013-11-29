4f02f3655e2784d02008be7436dd70388a32c5d8
Marian Steinbach <marian@sendung.de>
2013-11-29 08:56:40 +0100

    Experimental dataset: Points of Interest from OpenStreetMap

---
+++ data/geo-base/osm-pois/README.md
@@ -0,0 +1,14 @@
+Points of Interest from OpenStreetMap
+=====================================
+
+Some OpenStreetMap node data, grouped by amenity tag, provided as GeoJSON files.
+
+We use the 100 most frequent amenity tags and then look for occurrences within a bounding box around Cologne, Germany.
+
+## Source
+
+http://www.openstreetmap.org/
+
+## License
+
+OpenStreetMap is open data, licensed under the [Open Data Commons Open Database License (ODbL)](http://opendatacommons.org/licenses/odbl/).
---
+++ data/geo-base/osm-pois/download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Download list of popular "amenity" tags,
+# then download list of nodes for each tag
+# and store in GeoJSON files.
+
+# Bounding Box for Cologne
+BBOX=50.8,6.7,51.1,7.2
+
+# check some dependencies
+which in2csv || { echo "csvkit is not installed. Exiting."; exit 1; }
+which curl || { echo "curl is not installed. Exiting."; exit 1; }
+
+# Get popular tags and store as CSV
+curl -s -X GET -d "key=amenity&page=1&rp=100&sortname=count&sortorder=desc" \
+	http://taginfo.openstreetmap.org/api/4/key/values|in2csv -f json -k data \
+	>> tags_amenity.csv
+
+# Get nodes per tag
+for tag in $(csvcut -c 5 tags_amenity.csv|tail -n+2)
+do
+	echo "Fetching amenity:$tag"
+	curl -s -X POST -d "[out:json];(node[\"amenity\"=\"$tag\"]($BBOX));out;" \
+		http://overpass-api.de/api/interpreter \
+		|in2csv -f json -k elements \
+		|csvcut -c "id,lat,lon,tags/name" \
+		|csvjson --lat lat --lon lon -k id -i 4 \
+		>> amenity_$tag.geojson
+	sleep 1
+done
