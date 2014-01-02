04db92daa343dc24f648c75d8ea0c9ab38fae874
Marian Steinbach <marian@sendung.de>
2014-01-02 17:03:00 +0100

    Download URL update

--- data/elections/bundestag-2013/wahlvorschlaege/download.sh
+++ data/elections/bundestag-2013/wahlvorschlaege/download.sh
@@ -5,7 +5,7 @@
 #   sh ./download.sh
 
 
-SOURCE_URL="http://www.offenedaten-koeln.de/wp-content/plugins/download-monitor/download.php?id=86"
+SOURCE_URL="http://www.offenedaten-koeln.de/node/269/download"
 NAME=wahlvorschlaege
 FINAL_FOLDER=$NAME
 FILENAME=${NAME}.csv
