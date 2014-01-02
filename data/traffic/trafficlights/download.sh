c86f89d0383cb35898ad6029d3c1fe090b3b2b68
Marian Steinbach <marian@sendung.de>
2014-01-02 17:51:41 +0100

    Download URL update

--- data/traffic/trafficlights/download.sh
+++ data/traffic/trafficlights/download.sh
@@ -5,7 +5,7 @@
 #   sh ./download.sh
 
 
-SOURCE_URL="http://www.offenedaten-koeln.de/wp-content/plugins/download-monitor/download.php?id=100"
+SOURCE_URL="http://www.offenedaten-koeln.de/node/330/download"
 NAME="Ampelanlagen"
 FINAL_FOLDER=$NAME
 FILENAME=${NAME}.zip
