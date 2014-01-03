fedd1c949bf1719c5bb5d0654983204005f734ef
Marian Steinbach <marian@sendung.de>
2014-01-03 14:01:46 +0100

    Latest changes

--- data/environment/weather-dwd-2667/analysis.R
+++ data/environment/weather-dwd-2667/analysis.R
@@ -6,6 +6,8 @@
 # import dependencies
 require(zoo)
 require(ggplot2)
+require(splines)
+require(MASS)
 
 weather.df <- read.csv("./weather-dwd-2667.csv")
 colnames(weather.df) <- c("datum", "qual", "bedeckung", "relfeuchte",
@@ -37,8 +39,14 @@ weather.ltemp.zoo <- with(weather.df, zoo(ltemp, order.by = weather.df$datum))
 weather.ltemp.ts <- ts(weather.df$ltemp, frequency=365, start=c(1957, 243))
 ltemp.decomp <- decompose(weather.ltemp.ts)
 plot(ltemp.decomp$trend)
-ltemp.decomp.trend.df <- as.data.frame(ltemp.decomp$trend)
-ggplot(ltemp.decomp.trend.df$x, aes(x=x)) + geom_line()
+ltemp.decomp.trend.df <- as.data.frame(matrix(ltemp.decomp$trend))
+ltemp.decomp.trend.df$datum <- weather.df$datum
+ggplot(data=ltemp.decomp.trend.df, aes(x=datum, y = V1)) +
+  geom_line(weight=0.2) +
+  geom_smooth(method="lm", formula = y ~ ns(x,5), size=0.5) +
+  ggtitle("Air temperature trend") +
+  xlab("Date") +
+  ylab("Temperature (°C)")
 
 # Time series plot of air temperature
 ggplot(weather.df, aes(datum, ltemp)) + geom_line(size=0.2) + geom_smooth() + ggtitle("Air temperature Cologne/Bonn 1957 - 2013") + xlab("Date") + ylab("Temperature (°C)")
