b18a961efe5caf1c33caea9b1d21eb3e595b7507
Marian Steinbach <marian@sendung.de>
2014-01-15 17:08:26 +0100

    Now more source files due to file size

--- data/environment/lanuv-gauge/README.md
+++ data/environment/lanuv-gauge/README.md
@@ -5,6 +5,8 @@ This folder contains daily unverified water gauge values from various streams in
 
 Station information is contained in the files "gauge_stations.csv" and "gauge_stations.geojson". Station coordinates in the source appear to be coded in UTM32N format. They are converted to WGS84 (latitude/longitude) coordinates for the CSV and GeoJSON export.
 
+Source data is not contained in the repository due to file size restrictions. The Original files can be downloaded and derivatives updated using the script "download.sh".
+
 ## Source
 
 Landesamt für Natur, Umwelt und Verbraucherschutz (LANUV)
@@ -20,3 +22,5 @@ Unknown
 Dieser Ordner enthält ungeprüfte Pegeldaten des LANUV mit täglichen Werten ab Januar 2013.
 
 Stationsdaten sind in den Dateien "gauge_stations.csv" und "gauge_stations.geojson" enthalten. Die Stationskoordinaten in den Quelldaten sind im UTM32N-Format angegeben. Für die CSV- und Geo-JSON-Exporte werden sie in WGS84-Koordinaten (Längen- und Breitengrad) konvertiert.
+
+Aufgrund der Dateigrößen sind die Originaldateien nicht im Repository enthalten. Diese können jedoch temporär mit dem Script "download.sh" herunter geladen werden, um die Derivate zu aktualisieren.
--- data/environment/lanuv-gauge/download.sh
+++ data/environment/lanuv-gauge/download.sh
@@ -25,11 +25,13 @@ URL2="http://luadb.lds.nrw.de/LUA/hygon/messwerte/pegeldaten.tar.gz"
 #http://luadb.lds.nrw.de/LUA/hygon/messwerte/guetedaten.tar.gz
 
 # Load source files
+echo "Downloading source data..."
 rm -r _source
 mkdir -p _source/gauge
 curl -s $URL2 > _source/gauge/historic.tar.gz
 
 # unzip source files
+echo "Unzipping source data..."
 cd $ROOT/_source/gauge
 tar xzf historic.tar.gz
 rm historic.tar.gz
@@ -37,6 +39,7 @@ rm historic.tar.gz
 cd $ROOT
 
 # convert stations file
+echo "Creating station files..."
 rm -f gauge_stations_tmp.geojson
 in2csv -f csv -e latin1 _source/gauge/pegel_stationen.txt > stations_EPSG-32632.csv
 ogr2ogr -f GeoJSON gauge_stations_tmp.geojson gauge_stations_EPSG-32632.vrt -t_srs "EPSG:4326"
@@ -50,6 +53,7 @@ rm -f stations_EPSG-32632.csv
 rm -f stations_tmp.csv
 
 # append daily values to stored file
+echo "Creating value files..."
 tail -n+2 gauge_values_daily.csv > out.csv
 in2csv -f csv -e latin1 _source/gauge/pegel_tageswerte.txt|tail -n+2 >> out.csv
 sort -u out.csv > out_uniq.csv
@@ -57,3 +61,5 @@ rm out.csv
 head -n 1 _source/gauge/pegel_tageswerte.txt|in2csv -f csv -e latin1 > gauge_values_daily.csv
 cat out_uniq.csv >> gauge_values_daily.csv
 rm out_uniq.csv
+
+rm -r _source
