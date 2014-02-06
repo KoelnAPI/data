a1cb5d77ded30ff4605bfe3f8223835f6dcadf88
Marian Steinbach <marian@sendung.de>
2014-02-06 10:56:39 +0100

    Download of crowdsav POI data added

--- data/health/aed/download.sh
+++ data/health/aed/download.sh
@@ -9,12 +9,6 @@ FINAL_FOLDER=$NAME
 FILENAME=${NAME}.csv
 USER_AGENT="datahub-cgn/0.1"
 
-# creating SHA1 hash to compare before/after download
-if [ -a _source/$FILENAME ]; then
-    HASH=shasum.before
-    shasum _source/$FILENAME > $HASH
-fi
-
 echo "Downloading $NAME to _source/$FILENAME"
 
 cd _source
@@ -22,18 +16,20 @@ rm $FILENAME
 wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
 cd ..
 
-# Check SHA1 of new download
-if [ -a $HASH ]; then
-    shasum -c --status $HASH
-    if [ $? -eq 0 ]; then
-        echo "Source file hasn't changed"
-        rm $HASH
-        exit
-    else
-        echo "Source file has been updated"
-        rm $HASH
-    fi
-fi
-
 
 ### / ENDE Download der CSV-Datei der Stadt Köln
+
+
+### Download POIs von crowdsav.com
+
+SOURCE_URL="https://www.crowdsav.com/aeds/get_by_bounds?maxlat=51.097485&minlat=50.810274&maxlng=7.158737&minlng=6.753&limit=500&offset=0"
+FILENAME=aed_crowdsav.json
+
+echo "Downloading $NAME to _source/$FILENAME"
+cd _source
+rm $FILENAME
+wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
+cd ..
+
+# Konvertiere Crowdsav JSON zu CSV-Datei
+in2csv -f json -k pois _source/$FILENAME > aed_crowdsav.csv
