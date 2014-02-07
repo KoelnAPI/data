39109e30c7778a8c0f4c54da46be64e03fe14663
Marian Steinbach <marian@sendung.de>
2014-02-07 18:17:08 +0100

    Switch from wget to curl

--- data/transport/kvb-status/download.sh
+++ data/transport/kvb-status/download.sh
@@ -5,7 +5,11 @@
 
 SOURCE_URL="http://www.kvb-koeln.de/german/home/mofis_bahn.html"
 
+# Seems to be necessary for KVB
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36"
+#USER_AGENT="Mozilla/5.0"
+
 cd _source/
-wget -N -U --timeout=120 "Mozilla/1.0" $SOURCE_URL
+curl -R -A "Mozilla/5.0" $SOURCE_URL > mofis_bahn.html
 cd ..
 
