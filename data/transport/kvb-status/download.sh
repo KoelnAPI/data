67d499d0e6d4f3555cfca61d6841c6f9fcd06373
Marian Steinbach <marian@sendung.de>
2014-02-11 10:45:09 +0100

    Silent downloading

--- data/transport/kvb-status/download.sh
+++ data/transport/kvb-status/download.sh
@@ -7,7 +7,7 @@ SOURCE_URL1="http://www.kvb-koeln.de/german/home/mofis_bahn.html"
 SOURCE_URL2="http://www.kvb-koeln.de/german/home/mofis_bus.html"
 
 cd _source/
-curl -R -A "Mozilla/5.0" $SOURCE_URL1 > mofis_bahn.html
-curl -R -A "Mozilla/5.0" $SOURCE_URL2 > mofis_bus.html
+curl -s -R -A "Mozilla/5.0" $SOURCE_URL1 > mofis_bahn.html
+curl -s -R -A "Mozilla/5.0" $SOURCE_URL2 > mofis_bus.html
 cd ..
 
