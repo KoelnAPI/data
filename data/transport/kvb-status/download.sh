0e5a8b75bb975aa49c34790dbefece700e762806
Marian Steinbach <marian@sendung.de>
2014-02-11 09:53:42 +0100

    Now also tracking bus messages

--- data/transport/kvb-status/download.sh
+++ data/transport/kvb-status/download.sh
@@ -3,13 +3,11 @@
 # Download latest news from KVB website
 # and make it available as proper data
 
-SOURCE_URL="http://www.kvb-koeln.de/german/home/mofis_bahn.html"
-
-# Seems to be necessary for KVB
-USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36"
-#USER_AGENT="Mozilla/5.0"
+SOURCE_URL1="http://www.kvb-koeln.de/german/home/mofis_bahn.html"
+SOURCE_URL2="http://www.kvb-koeln.de/german/home/mofis_bus.html"
 
 cd _source/
-curl -R -A "Mozilla/5.0" $SOURCE_URL > mofis_bahn.html
+curl -R -A "Mozilla/5.0" $SOURCE_URL1 > mofis_bahn.html
+curl -R -A "Mozilla/5.0" $SOURCE_URL2 > mofis_bus.html
 cd ..
 
