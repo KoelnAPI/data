e3871375ee5b5b91e95c2bd7305e3015a3b70a5e
Marian Steinbach <marian@sendung.de>
2014-02-11 10:37:04 +0100

    New script for statistics tracking

---
+++ data/transport/kvb-status/track_message_status.py
@@ -0,0 +1,76 @@
+# encoding: utf-8
+
+"""
+Track message status to database
+for later evaluation
+"""
+
+import json
+import MySQLdb
+import warnings
+from datetime import datetime
+
+
+def init_db():
+    warnings.filterwarnings('ignore')
+    sql = """
+    CREATE TABLE IF NOT EXISTS kvbstatus_bahn (
+        datetime_local DATETIME UNIQUE,
+        num_messages SMALLINT,
+        num_stations MEDIUMINT
+    )
+    """
+    cursor.execute(sql)
+    sql = """
+    CREATE TABLE IF NOT EXISTS kvbstatus_bus (
+        datetime_local DATETIME UNIQUE,
+        num_messages SMALLINT,
+        num_stations MEDIUMINT
+    )
+    """
+    cursor.execute(sql)
+
+
+def track(path_bahn, path_bus):
+    bahn = json.load(open(path_bahn, "rb"))
+    bus = json.load(open(path_bus, "rb"))
+    lastmod_bahn = datetime.strptime(bahn["last_modified"], "%Y-%m-%dT%H:%M:%S")
+    lastmod_bus = datetime.strptime(bus["last_modified"], "%Y-%m-%dT%H:%M:%S")
+    num_messages_bahn = 0
+    num_messages_bus = 0
+    num_stations_bahn = 0
+    num_stations_bus = 0
+
+    for msg in bahn["messages"]:
+        num_messages_bahn += 1
+        for station in msg["station_info"]:
+            num_stations_bahn += 1
+    cursor.execute("INSERT IGNORE INTO kvbstatus_bahn VALUES (%s, %s, %s)",
+        (lastmod_bahn, num_messages_bahn, num_stations_bahn))
+    for msg in bus["messages"]:
+        num_messages_bus += 1
+        for station in msg["station_info"]:
+            num_stations_bus += 1
+    cursor.execute("INSERT IGNORE INTO kvbstatus_bus VALUES (%s, %s, %s)",
+        (lastmod_bus, num_messages_bus, num_stations_bus))
+
+    print("Bahn: %d Meldungen, %d Stationen - %s" % (num_messages_bahn, num_stations_bahn, lastmod_bahn))
+    print("Bus: %d Meldungen, %d Stationen - %s" % (num_messages_bus, num_stations_bus, lastmod_bus))
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', dest="dbhost",
+        help='DB host name. Default: localhost', default="localhost")
+    parser.add_argument('-u', dest="dbuser",
+        help='DB user name. Default: koelnapi', default="koelnapi")
+    parser.add_argument('-p', dest="dbpass",
+        help='DB password. Default: empty', default="")
+    parser.add_argument('-n', dest="dbname",
+        help='DB name. Default: koelnapi', default="koelnapi")
+    args = parser.parse_args()
+    conn = MySQLdb.connect(host=args.dbhost,
+        user=args.dbuser, passwd=args.dbpass, db=args.dbname)
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    init_db()
+    track("messages_bahn.json", "messages_bus.json")
