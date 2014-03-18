4c2851840367ba6a7fa516412c8bc67eb0642786
Marian Steinbach <marian@sendung.de>
2014-03-18 12:26:50 +0100

    Bugfix in download.sh

--- data/environment/uba-airquality/download.sh
+++ data/environment/uba-airquality/download.sh
@@ -7,7 +7,7 @@ wget -O stations_raw.csv "http://www.env-it.de/stationen/public/download.do?even
 tail -n+2 stations_raw.csv|in2csv -d ";" -e "ISO-8859-1" -f csv|csvcut -C "dem_status,_unnamed" > stations_complete.csv
 
 # create list with active stations only (no end date)
-csvgrep -c "station_end_date" -m "" stations_complete.csv > stations_active.csv
+csvgrep -c "station_end_date" -r "[0-9]" -i stations_complete.csv > stations_active.csv
 
 # remove rest
 rm stations_raw.csv
