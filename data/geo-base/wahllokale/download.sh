9ff4063279ec45590198c95cb866e86b72eb8bb7
marians <marian@Marians-MacBook-Pro.local>
2014-05-18 11:51:13 +0200

    Added GeoJSON conversion

--- data/geo-base/wahllokale/download.sh
+++ data/geo-base/wahllokale/download.sh
@@ -5,7 +5,7 @@
 #   sh ./download.sh
 
 
-SOURCE_URL="http://www.offenedaten-koeln.de/node/289/download"
+SOURCE_URL="http://www.offenedaten-koeln.de/sites/default/files/20140513_Wahllokale_geo.csv"
 NAME="wahllokale"
 FINAL_FOLDER=$NAME
 FILENAME=${NAME}.csv
@@ -19,8 +19,16 @@ cd ./_source/
 wget -q -U $USER_AGENT -O $FILENAME $SOURCE_URL
 cd ..
 
-# remove dots from within figures, replace seperator
-echo "Creating improved version in $FILENAME"
-perl -CSD -pe 'tr/\x{feff}//d' _source/$FILENAME > $FILENAME.tmp
-cat $FILENAME.tmp|in2csv -f csv -d ";"  > $FILENAME
-rm $FILENAME.tmp
+# Create temporary GeoJSON
+ogr2ogr -f GeoJSON \
+	wahllokale_tmp.geojson \
+	wahllokale.vrt \
+	-t_srs "EPSG:4326"
+
+# set 7 digits float precision, sort, indent
+python ../../../tools/lessprecise/lessprecise.py \
+	--indent 4 --sort 1 -o wahllokale.geojson \
+	wahllokale_tmp.geojson
+
+# remove temp GeoJSON file
+rm wahllokale_tmp.geojson
